ac5b61bcc2fd6fd900ca852c6f0351d9278e3d4d
Spencer Weiss <spweiss@seas.upenn.edu>
2017-07-26 17:07:29 -0400

    Update cost.py

--- cost.py
+++ cost.py
@@ -1,3 +1,21 @@
-import revenue
+import revenue, math
 
-revenue.total_sales
+# Determines the cost due to technicians each year where one can make 840000 devices per year and is paid 36,000 USD
+techs = []
+for x in range(0,5):
+  techs.append(36000*math.ceil(revenue.total_sales/840000))
+
+# Determines the cost of production each year where unit production cost is 2 USD
+material = []
+for x in range(0,5):
+  material.append(2*revenue.total_sales)
+
+# Determines the cost due to sales representatives each year where each is paid 36,000 USD
+sales = [0,1,2,4,5]
+for x in range(0,5):
+  sales[x] = sales[x]*2
+
+# Finds the total cost each year
+total_cost = []
+for x in range(0,5):
+  total_cost.append(techs[x]+material[x]+sales[x])
