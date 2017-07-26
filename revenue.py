27d0f1f1de146df98e23c76b259db92663fb395c
Spencer Weiss <spweiss@seas.upenn.edu>
2017-07-26 15:03:57 -0400

    Update revenue.py

--- revenue.py
+++ revenue.py
@@ -1,10 +1,10 @@
 import rates, volumes, sales
 
 # Sums sale volume for a particular test and clinic type over a five-year range based on 
-def SaleVolume(volume, salerate, clinicrate, ...):
+def SaleVolume(volume, rate, sale, ...):
   salelist = []
   total = 0
   clinics = 0
   for x in range(0,12):
-    clinics += clinicrate
-    total += volume*salerate*clinicrate
+    clinics += sale
+    total += volume*salerate*sale
